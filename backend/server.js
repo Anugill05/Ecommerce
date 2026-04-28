b3e3ba3ce82e2d6ab57027b2215499cf7e296d73
Anu gill <gillanu2003@gmail.com>
2026-04-28 22:43:58 +0530

    update changes

--- backend/server.js
+++ backend/server.js
@@ -210,7 +210,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin:       process.env.CLIENT_URL || "https://ecommerce-1-tlbi.onrender.com",
+  origin:       process.env.CLIENT_URL || "https://ecommerce-1-782g.onrender.com",
   credentials:  true,
   methods:      ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
